41cfde7e4f9d89f0f692a86bc3ee09f551723f6c
Akira Ogiso <ogiso.a@5sg.jp>
2013-09-24 17:01:08 +0900

    aaa

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,7 +1,13 @@
-class ListController < ApplicationController
+#  -- coding: utf-8 --  #
 
+class ListController < ApplicationController
+=begin
   def hello
-    render:text => 'Hello!'
+    render "list/hello"
+  end
+=end
+  def hello
+    @msg='こんにちは。お久しぶりです。';
   end
 
   def bye
